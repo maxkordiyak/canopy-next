4885b07d2096c8c940057a13c0279015aaba6ad5
Maksym Kordiyak <maksym.kordiyak@lemberg.co.ua>
2018-04-26 13:55:54 +0300

    Add scss compiler

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ class Home extends React.Component {
       <div className="Home">
         <div className="Home-header">
           <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to azasdasdzle</h2>
+          <h2>Welcome to test</h2>
         </div>
         <p className="Home-intro">
           To get started, edit <code>src/App.js</code> or{' '}
