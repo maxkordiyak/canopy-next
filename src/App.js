e169db4ff157b2bcc17fdd4b7ca5682fa2be0035
Maksym Kordiyak <maksym.kordiyak@lemberg.co.ua>
2018-04-26 14:35:26 +0300

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import CssBaseline from 'material-ui/CssBaseline';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from './Home';
 import './App.css';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
+    <React.Fragment>
+        <CssBaseline />
+        <Switch>
+            <Route exact path="/" component={Home} />
+        </Switch>
+    </React.Fragment>
 );
 
 export default App;
