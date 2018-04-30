21716373b3f99fa8132a3dea62e9f55be5857043
Maksym Plotnikov <maksym.plotnikov@lemberg.co.uk>
2018-04-30 16:35:39 +0300

    Add Multisection Autosuggest with RegExp validation

--- src/components/Autocomplete/index.js
+++ src/components/Autocomplete/index.js
@@ -6,46 +6,77 @@ import parse from 'autosuggest-highlight/parse';
 import TextField from 'material-ui/TextField';
 import Paper from 'material-ui/Paper';
 import { MenuItem } from 'material-ui/Menu';
+import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 import { InputAdornment } from 'material-ui/Input';
 import Search from '@material-ui/icons/Search';
 import { CircularProgress } from 'material-ui/Progress';
 
 const suggestions = [
-	{ label: 'Afghanistan' },
-	{ label: 'Aland Islands' },
-	{ label: 'Albania' },
-	{ label: 'Algeria' },
-	{ label: 'American Samoa' },
-	{ label: 'Andorra' },
-	{ label: 'Angola' },
-	{ label: 'Anguilla' },
-	{ label: 'Antarctica' },
-	{ label: 'Antigua and Barbuda' },
-	{ label: 'Argentina' },
-	{ label: 'Armenia' },
-	{ label: 'Aruba' },
-	{ label: 'Australia' },
-	{ label: 'Austria' },
-	{ label: 'Azerbaijan' },
-	{ label: 'Bahamas' },
-	{ label: 'Bahrain' },
-	{ label: 'Bangladesh' },
-	{ label: 'Barbados' },
-	{ label: 'Belarus' },
-	{ label: 'Belgium' },
-	{ label: 'Belize' },
-	{ label: 'Benin' },
-	{ label: 'Bermuda' },
-	{ label: 'Bhutan' },
-	{ label: 'Bolivia, Plurinational State of' },
-	{ label: 'Bonaire, Sint Eustatius and Saba' },
-	{ label: 'Bosnia and Herzegovina' },
-	{ label: 'Botswana' },
-	{ label: 'Bouvet Island' },
-	{ label: 'Brazil' },
-	{ label: 'British Indian Ocean Territory' },
-	{ label: 'Brunei Darussalam' }
+	{
+		title: 'Properties',
+		suggestions: [
+			{
+				name: 'Covey Cl, Lichfield WS13 6BS, UK',
+				info: 'House'
+			},
+			{
+				name: 'Stevenage Rd, St Ippolyts, Hitchin SG4 9YD, UK',
+				info: 'Apt'
+			}
+		]
+	},
+	{
+		title: 'Renters',
+		suggestions: [
+			{
+				name: 'Daisy Tester',
+				info: 'UK'
+			},
+			{
+				name: 'Donald Tester',
+				info: 'USA'
+			}
+		]
+	},
+	{
+		title: 'Landlords',
+		suggestions: [
+			{
+				name: 'Michael Landlord',
+				info: 'UK'
+			},
+			{
+				name: 'John Landlord',
+				info: 'UK'
+			},
+			{
+				name: 'Sarah Landlord',
+				info: 'UK'
+			},
+			{
+				name: 'Noah Landlord',
+				info: 'UK'
+			}
+		]
+	},
+	{
+		title: 'Users',
+		suggestions: [
+			{
+				name: 'Anonymous User',
+				info: 'UK'
+			},
+			{
+				name: 'Test User',
+				info: 'UK'
+			},
+			{
+				name: 'Friend User',
+				info: 'UK'
+			}
+		]
+	}
 ];
 
 
@@ -84,8 +115,7 @@ const styles = theme => ({
 });
 
 function renderInput(inputProps) {
-	const { classes, ref, ...other } = inputProps;
-
+	const { classes, ref, loading, ...other } = inputProps;
 	return (
 		<div className={classes.flexRow}>
 			<TextField
@@ -94,19 +124,18 @@ function renderInput(inputProps) {
 					classes: {
 						input: classes.searchInput
 					},
-					startAdornment: <InputAdornment position="start"><Search /></InputAdornment>,
+					startAdornment: <InputAdornment position="start"><Search/></InputAdornment>,
 					...other
 				}}
 			/>
-			<CircularProgress size={24}/>
+			{loading ? <CircularProgress size={24}/> : null}
 		</div>
 	);
 }
 
-function renderSuggestion(suggestion, { query, isHighlighted }) {
-	const matches = match(suggestion.label, query);
-	const parts = parse(suggestion.label, matches);
-
+const renderSuggestion = (suggestion, { query, isHighlighted }) => {
+	const matches = match(suggestion.name, query);
+	const parts = parse(suggestion.name, matches);
 	return (
 		<MenuItem selected={isHighlighted} component="div">
 			<div>
@@ -121,91 +150,121 @@ function renderSuggestion(suggestion, { query, isHighlighted }) {
 						</strong>
 					);
 				})}
+				<span> &nbsp;({suggestion.info})</span>
 			</div>
 		</MenuItem>
 	);
+};
+
+function escapeRegexCharacters(str) {
+	return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function renderSectionTitle(section) {
+	return (
+		<Typography style={{
+			padding: '0 16px',
+			fontSize: 12,
+			fontWeight: 700
+		}} variant="body2" gutterBottom>{section.title}</Typography>
+	);
+}
+
+function getSectionSuggestions(section) {
+	return section.suggestions;
 }
 
 function renderSuggestionsContainer(options) {
 	const { containerProps, children } = options;
-
 	return (
-		<Paper {...containerProps} square>
+		<Paper {...containerProps}>
 			{children}
 		</Paper>
 	);
 }
 
 function getSuggestionValue(suggestion) {
-	return suggestion.label;
+	return suggestion.name;
 }
 
 function getSuggestions(value) {
-	const inputValue = value.trim().toLowerCase();
-	const inputLength = inputValue.length;
-	let count = 0;
-
-	return inputLength === 0
-		? []
-		: suggestions.filter(suggestion => {
-			const keep =
-				count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
-
-			if (keep) {
-				count += 1;
-			}
-
-			return keep;
-		});
+	const escapedValue = escapeRegexCharacters(value);
+	if (escapedValue === '') {
+		return [];
+	}
+	const regex = new RegExp(`\\b${escapedValue}`, 'g' );
+	console.log(regex);
+	return suggestions.map(section => {
+		return {
+			title: section.title,
+			suggestions: section.suggestions.filter(suggestion => regex.test(suggestion.name))
+		};
+	})
+		.filter(section => section.suggestions.length > 0);
 }
+
 
 class IntegrationAutosuggest extends React.Component {
 	state = {
 		value: '',
-		suggestions: []
+		suggestions: [],
+		isLoading: 0
 	};
-
+	
 	handleSuggestionsFetchRequested = ({ value }) => {
-		this.setState({
-			suggestions: getSuggestions(value)
-		});
+		// FIXME Fetching emulations - REMOVE
+		setTimeout(() => {
+			this.setState({
+				suggestions: getSuggestions(value),
+				isLoading: 0
+			});
+		}, 1500);
+		
 	};
-
+	
 	handleSuggestionsClearRequested = () => {
 		this.setState({
-			suggestions: []
+			suggestions: [],
+			isLoading: 0
 		});
 	};
-
+	
 	handleChange = (event, { newValue }) => {
 		this.setState({
-			value: newValue
+			value: newValue,
+			isLoading: 1
 		});
 	};
-
+	
 	render() {
 		const { classes } = this.props;
+		const inputProps = {
+			classes,
+			loading: this.state.isLoading,
+			placeholder: 'Search Properties, Renters, Landlords & Users...',
+			value: this.state.value,
+			onChange: this.handleChange
+		};
 		return (
 			<Autosuggest
 				theme={{
 					container: classes.container,
 					suggestionsContainerOpen: classes.suggestionsContainerOpen,
 					suggestionsList: classes.suggestionsList,
-					suggestion: classes.suggestion
+					suggestion: classes.suggestion,
+					suggestionTitle: classes.suggestionTitle
 				}}
-				renderInputComponent={renderInput}
+				multiSection={true}
 				suggestions={this.state.suggestions}
+				renderInputComponent={renderInput}
 				onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
 				onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
 				renderSuggestionsContainer={renderSuggestionsContainer}
 				getSuggestionValue={getSuggestionValue}
 				renderSuggestion={renderSuggestion}
-				inputProps={{
-					classes,
-					placeholder: 'Search Properties, Renters, Landlords & Users...',
-					value: this.state.value,
-					onChange: this.handleChange
-				}}
+				getSectionSuggestions={getSectionSuggestions}
+				renderSectionTitle={renderSectionTitle}
+				inputProps={inputProps}
 			/>
 		);
 	}
@@ -213,6 +272,7 @@ class IntegrationAutosuggest extends React.Component {
 
 IntegrationAutosuggest.propTypes = {
 	classes: PropTypes.object.isRequired
+	
 };
 
 export default withStyles(styles)(IntegrationAutosuggest);
